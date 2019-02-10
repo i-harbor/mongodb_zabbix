a24705fa63703dc11fe710135ae0cfe0f0e1262f
radiance013 <2452711022@qq.com>
2019-02-10 22:51:37 +0800

    update

---
+++ monitoring for Replica Set/create_host_repl.py
@@ -0,0 +1,322 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+'''
+'@file: create_host_repl.py
+'@author: liyunting
+'@version: 1
+'@lastModify: 2019-02-10 21:09
+'
+'''
+
+import json
+import sys
+import getopt
+import requests
+
+
+def zabbix_call(payload, zabbix_server):
+	'''Call zabbix web api by json-rpc.
+
+	Args:
+		payload        dict    the parameter to be passed  
+		zabbix_server  string  the ip of zabbix server 
+
+	Returns:
+		the request response object  
+	'''
+	url = "http://" + zabbix_server + "/zabbix/api_jsonrpc.php"
+	headers = {'content-type': 'application/json'}
+	response = requests.post(url, data=json.dumps(payload), headers=headers).json()
+	return response
+
+
+def zabbix_auth(user, pwd, zabbix_server):
+	'''Connect to zabbix server and authenticate by the given username and password.
+
+	Args:
+		user           string   the zabbix server username
+		pwd            string   the zabbix server password
+		zabbix_server  string   the ip of zabbix server
+
+	Returns:
+		the authentication token
+	'''
+	auth = ''
+	payload = {
+	    "method": "user.login",
+	    "params": {
+	    	"user": user,
+	    	"password": pwd
+	    },
+	    "jsonrpc": "2.0",
+	    "id": 1,
+	    "auth": None
+	}
+	res = zabbix_call(payload, zabbix_server) 
+	if 'result' in res :
+		auth = res['result']
+	else:
+		print(res['error'])
+	return auth
+
+
+def zabbix_create_group(auth, groupname, zabbix_server):
+	'''Connect to zabbix server and create host group.
+
+	If the host group exists, it will be failed. And if it does not exist, then create it and return
+	the id of the host group.
+
+	Args:
+		auth           string   your authentication token
+		groupname      string   the name of the hostgroup
+		zabbix_server  string   the ip of zabbix server
+
+	Returns:
+		the host group id
+	'''
+	host_group_id = ''
+	payload = {
+		"jsonrpc": "2.0",
+		"method": "hostgroup.create",
+		"params": {
+		    "name": groupname
+		},
+		"auth": auth,
+		"id": 1
+	}
+	res = zabbix_call(payload, zabbix_server)
+	if 'result' in res :
+		host_group_id = res['result']['groupids'][0]
+		print('create hostgroup:', groupname, 'successfully')
+	else:
+		print("error in creating hostgroup:", groupname)
+		print(res['error'])
+	return host_group_id
+
+
+def zabbix_import_template(auth, filepath, zabbix_server):
+	'''Connect to zabbix server and import the MongoDB template.
+
+	Args:
+		auth          string   your authentication token
+		filepath      string   the whole path of the xml file(e.g. '/root/liyunting/example.xml')
+		zabbix_server string   the ip of zabbix server
+	'''
+	with open(filepath,'r') as f:
+		content = f.read()
+	payload = {
+		"jsonrpc": "2.0",
+		"method": "configuration.import",
+		"params": {
+			"format": "xml",
+			"rules": {
+				"applications": {
+						"createMissing": True,
+						"deleteMissing": False
+				},
+				"templates": {
+					"createMissing": True,
+					"updateExisting": True
+				},
+				"screens": {
+					"createMissing": True,
+					"updateExisting": True
+				},
+				"valueMaps": {
+					"createMissing": True,
+					"updateExisting": False
+				},
+				"graphs": {
+					"createMissing": True,
+					"updateExisting": True,
+					"deleteMissing": True
+				},
+				"triggers": {
+					"createMissing": True,
+					"updateExisting": True,
+					"deleteMissing": True
+				},
+				"items": {
+					"createMissing": True,
+					"updateExisting": True,
+					"deleteMissing": True
+				}
+			},
+			"source": content
+		},
+		"auth": auth,
+		"id": 1
+	}
+	res = zabbix_call(payload, zabbix_server)
+	if 'result' in res :
+		if res['result']:
+			print('template import successfully')
+	else :
+		print('error in importing the template')
+		print(res['error'])
+
+
+def zabbix_get_template(auth, templatename, zabbix_server):
+	'''Connect to zabbix server and get the id of the template.
+
+	Args:
+		auth          string   your authentication token
+		templatename  string   the name of the template
+		zabbix_server string   the ip of zabbix server
+
+	Returns:
+		the template id
+	'''
+	template_id = ''
+	if auth != '':
+		payload = {
+    		"jsonrpc": "2.0",
+    		"method": "template.get",
+    		"params": {
+    		    "output": "extend",
+    		    "filter": {
+    		        "host": [
+    		            templatename
+    		        ]
+    		    }
+    		},
+    		"auth": auth,
+    		"id": 1
+		}
+		res = zabbix_call(payload, zabbix_server)
+		if 'result' in res :
+			template_id = res['result'][0]['templateid']
+		else:
+			print(res['error'])
+	return template_id
+
+
+def zabbix_create_host(auth, hostname, hostip, host_group_id, template_id, zabbix_server):
+	'''Create host and link template.
+
+	Args:
+		auth          string    your authentication token
+		hostname      string    the name of the host to be created
+		hostip        string    the ip of the host to be created
+		host_group_id string    the id of host group
+		template_id   string    the id of template
+		zabbix_server string    the ip of zabbix server
+	'''
+	host_id = ''
+	payload = {
+	    "jsonrpc": "2.0",
+	    "method": "host.create",
+	    "params": {
+	        "host": hostname,
+	        "interfaces": [
+	            {
+	                "type": 1,
+	                "main": 1,
+	                "useip": 1,
+	                "ip": hostip,
+	                "dns": "",
+	                "port": "10050"
+	            }
+	        ],
+	        "groups": [
+	            {
+	                "groupid": host_group_id
+	            }
+	        ],
+	        "templates": [
+	            {
+	                "templateid": template_id
+	            }
+	        ]		        
+	    },
+	    "auth": auth,
+	    "id": 1
+	}
+	res = zabbix_call(payload, zabbix_server)
+	if 'result' in res :
+		host_id = res['result']['hostids'][0]
+		print("create host:", hostname , "successfully")
+	else:
+		print('error in creating host:', hostname)
+		print(res['error'])
+
+
+def parseArg(argv):
+	'''Parse python command line arguments and return arguments.
+
+	Args:
+		argv   string  command line arguments
+
+	Returns:
+		zabbix_server  string  the ip of zabbix server
+		zabbix_user    string  the user of zabbix, default: Admin
+		zabbix_pwd     string  password for user, default: zabbix
+	'''
+	zabbix_user = 'Admin'
+	zabbix_pwd = 'zabbix'
+	zabbix_server = ''
+	try:
+		opts, args = getopt.getopt(argv,"hz:u:p:",["help"])
+	except getopt.GetoptError:
+		print('invalid option\nplease use python create_host_repl.py --help for more information\n')
+		sys.exit(2)
+	for opt, arg in opts:
+		if opt in ('-h', '--help'):
+			print('usage:\n  python create_host_repl.py -z <zabbix_server_ip> -u <zabbix_user> -p <zabbix_password>\n')
+			print('  if no user and password input, then user:Admin and password:zabbix will be used by default')
+			sys.exit()
+		elif opt == '-z':
+			zabbix_server = arg
+		elif opt == '-u':
+			zabbix_user = arg
+		elif opt == '-p':
+			zabbix_pwd = arg
+	return zabbix_server, zabbix_user, zabbix_pwd
+
+
+# the main method
+def main(argv):
+	zabbix_server, user, pwd = parseArg(argv)
+	if zabbix_server == '':
+		print('invalid input!\nplease check and use python create_host_repl.py --help for more information\n')
+		sys.exit(2)
+
+	auth = zabbix_auth(user, pwd, zabbix_server)
+	if auth == '':
+		print('\nzabbix server authentication failed\n')
+		sys.exit()
+
+	#import template
+	zabbix_import_template(auth, './mongodb_repl_arbiter.xml', zabbix_server)
+	zabbix_import_template(auth, './mongodb_repl_notarbiter.xml', zabbix_server)
+	arbiter_template_id = zabbix_get_template(auth, 'Template MongoDB Repl Arbiter', zabbix_server)	
+	notarbiter_template_id = zabbix_get_template(auth, 'Template MongoDB Repl Notarbiter', zabbix_server)
+
+	#read the json file
+	with open('./repl.json', 'r') as f:
+		repl = json.load(f)
+	members = repl['members']
+	name = repl['name']
+
+	#create host group 
+	group_id = zabbix_create_group(auth, 'Mongodb Repl ' + name, zabbix_server)
+	hostname_first = 'repl_'
+
+
+	if group_id != '' and arbiter_template_id != '' and notarbiter_template_id != '':
+		for m in members:
+			mongo_ip = m['ip']
+			role = m['role']
+			hostname = hostname_first + m['ip']
+			if role == 'not arbiter':
+				zabbix_create_host(auth, hostname, mongo_ip, group_id, notarbiter_template_id, zabbix_server)
+			if role == 'arbiter':
+				zabbix_create_host(auth, hostname, mongo_ip, group_id, arbiter_template_id, zabbix_server)
+	else:
+		print("can not complete creating all the hosts in your replica set, please check and try again")
+
+
+
+if __name__ == '__main__':
+	main(sys.argv[1:])
---
+++ monitoring for Replica Set/mongodb_repl_noauth.py
@@ -0,0 +1,187 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+'''
+'@file: mongodb_repl_noauth.py
+'@author: liyunting
+'@version: 1
+'@lastModify: 2019-02-10 22:06
+'
+'''
+
+
+import json
+import sys, getopt
+import subprocess
+from pymongo import *
+from pymongo.errors import ConnectionFailure
+from pymongo.errors import OperationFailure
+
+
+#the prefix of hosts that are created
+hostname_first = 'repl_'
+
+
+def getServerStatus(ip, port):
+	'''Get the serverStatus of the mongod instance.
+
+	Try to get the information of mongodb server by command.
+	You can refer to MongoDB manual for more details about serverStatus command.
+
+	Args:
+		ip    string   the server ip that the mongod instance is located
+		port  int      the port that is used by the mongod instance
+
+	Returns:
+		status code    int    0/1/2
+		server_status  dict   the status result
+	'''
+	server_status = {}
+	try:
+		client = MongoClient(ip, port)
+		db = client.admin
+		is_master = db.command('ismaster')
+		server_status = db.command('serverStatus')
+		return 0, server_status
+	except ConnectionFailure:
+		return 1, server_status
+	except OperationFailure:
+		return 2, server_status
+
+
+def getArbiterStatus(ip, port):
+	'''Get the status of an arbiter.
+
+	Try to get the information of mongodb server by command.
+	You can refer to MongoDB manual for more details about ismaster command.
+
+	Args:
+		ip    string   the ip of the arbiter
+		port  int      the port that is used by the arbiter
+
+	Returns:
+		status code    int    0/1
+	'''
+	try:
+		client = MongoClient(ip, port)
+		db = client.admin
+		is_master = db.command('ismaster')
+		return 0
+	except ConnectionFailure:
+		return 1
+
+
+def parseArg(argv):
+	'''Parse python command line arguments and return arguments.
+
+	Args:
+		argv   string  command line arguments
+
+	Returns:
+		zabbix_server  string  the ip of zabbix server
+	'''
+	zabbix_server = ''
+	try:
+		opts, args = getopt.getopt(argv,"hz:",["help"])
+	except getopt.GetoptError:
+		print('invalid option\nplease use python mongodb_repl_noauth.py --help for more information\n')
+		sys.exit(2)
+	for opt, arg in opts:
+		if opt in ('-h', '--help'):
+			print('usage:\n  python mongodb_repl_noauth.py -z <zabbix_server_ip>\n')
+			sys.exit()
+		elif opt == '-z':
+			zabbix_server = arg
+	return zabbix_server
+
+
+def send_value(item_key, zabbix_server, item_value, zabbix_host):
+	'''Send certain value to a zabbix host by zabbix_sender process.
+
+	Args:
+		item_key      string   the key of a certain zabbix item
+		zabbix_server string   the ip of zabbix server
+		item_value    string   the value to be send to zabbix server
+		zabbix_host   string   the hostname in zabbix
+	'''
+	send = subprocess.getstatusoutput('zabbix_sender -z ' + zabbix_server + ' -s ' + zabbix_host + ' -k ' + item_key + ' -o ' + item_value)
+	print(send[1])
+	if send[0] == 0:
+		print('\n', item_key, zabbix_host, 'send successfully\n')
+	else:
+		print('\n', item_key, zabbix_host, 'failed to send\n')
+
+
+def process_notarbiter(ip, port, zabbix_server, hostname):
+	'''Get the status data from a primary or secondary member of the replica set and send them to zabbix server.
+
+	And you can refer to MongoDB manual for more details about the returns of serverStatus command.
+
+	Args:
+		ip            string   the ip of mongo server
+		port          int      the port of mongo server
+		zabbix_server string   the ip of zabbix server
+		hostname      string   the host name in zabbix
+	'''
+	status_result =  getServerStatus(ip, port)
+	if status_result[0]  == 0:
+		send_value('mongo.alive', zabbix_server, str(1), hostname)
+		send_value('mongo.conn.current', zabbix_server, str(status_result[1]['connections']['current']), hostname)
+		send_value('mongo.conn.available', zabbix_server, str(status_result[1]['connections']['available']), hostname)
+		send_value('mongo.mem.resident', zabbix_server, str(status_result[1]['mem']['resident']), hostname)
+		send_value('mongo.network.in', zabbix_server, str(status_result[1]['network']['bytesIn']), hostname)
+		send_value('mongo.network.out', zabbix_server, str(status_result[1]['network']['bytesOut']), hostname)
+		send_value('mongo.op.delete', zabbix_server, str(status_result[1]['opcounters']['delete']), hostname)
+		send_value('mongo.op.getmore', zabbix_server, str(status_result[1]['opcounters']['getmore']), hostname)
+		send_value('mongo.op.insert', zabbix_server, str(status_result[1]['opcounters']['insert']), hostname)
+		send_value('mongo.op.query', zabbix_server, str(status_result[1]['opcounters']['query']), hostname)
+		send_value('mongo.op.update', zabbix_server, str(status_result[1]['opcounters']['update']), hostname)
+		send_value('mongo.page_faults', zabbix_server, str(status_result[1]['extra_info']['page_faults']), hostname)
+		send_value('mongo.uptime', zabbix_server, str(status_result[1]['uptime']), hostname)
+		send_value('mongo.version', zabbix_server, str(status_result[1]['version']), hostname)
+	elif status_result[0]  == 1:
+		send_value('mongo.alive', zabbix_server, str(0), hostname)
+		print('Cound not connect to the server', ip, str(port))
+	else:
+		print('\nCound not get the server status', ip, str(port))
+
+
+def process_arbiter(ip, port, zabbix_server, hostname):
+	'''Get the status data from an arbiter member of the replica set and send them to zabbix server.
+
+	Args:
+		ip            string   the ip of mongo server
+		port          int      the port of mongo server
+		zabbix_server string   the ip of zabbix server
+		hostname      string   the host name in zabbix
+	'''
+	status =  getArbiterStatus(ip, port)
+	if status  == 0:
+		send_value('mongo.alive', zabbix_server, str(1), hostname)
+	elif status  == 1:
+		send_value('mongo.alive', zabbix_server, str(0), hostname)
+		print('Cound not connect to the server', ip, str(port))
+
+
+# the main method 
+def main(argv):
+	zabbix_server = parseArg(argv)
+	if zabbix_server == '':
+		print('invalid input!\nplease check and use python mongodb_repl_noauth.py --help for more information\n')
+		sys.exit(2)
+
+	with open('/root/liyunting/repl.json', 'r') as f:
+		repl = json.load(f)
+	members = repl['members']
+	for m in members:
+		mongo_ip = m['ip']
+		mongo_port = m['port']
+		role = m['role']
+		if role == 'not arbiter':
+			process_notarbiter(mongo_ip, mongo_port, zabbix_server, hostname_first + mongo_ip)
+		if role == 'arbiter':
+			process_arbiter(mongo_ip, mongo_port, zabbix_server, hostname_first + mongo_ip)
+		
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
